3036d07f830846c3217cf99bc855003a8840d6d0
Lautaro Pereyra <lautaro.pereyraaaet32@gmail.com>
2026-05-08 13:58:19 -0300

    Update exquisite_jofo1.ino

--- exquisite_jofo1.ino
+++ exquisite_jofo1.ino
@@ -7,7 +7,6 @@ int cel;
 int valorluz;
 void setup()
 {
-
   Serial.begin(9800);
   pinMode(R,OUTPUT);
   pinMode(A,OUTPUT);
@@ -25,21 +24,25 @@ void loop()
   cel = map(((temp - 20) * 3.04), 0, 1023, -40, 125);
   Serial.println(cel);
   delay(1000);
-  if (valorluz > 30 && valorluz < 70){
-  if (cel > 90) {
-    analogWrite(R,255);
-     delay(1000);
+  if (valorluz > 30 && valorluz < 70)
+  {
+    if (cel > 90) 
+    {
+      analogWrite(R,255);
+      delay(1000);
       analogWrite(R,0);
-  }
-  else if (cel <= 18){
-    analogWrite(A,255);
+    }
+    else if (cel <= 18)
+    {
+      analogWrite(A,255);
       delay(1000);
       analogWrite(A,0);
-  }
-  else if (cel > 18 && cel <= 90){
-    analogWrite(V,255);
-       delay(1000);
+    }
+    else if (cel > 18 && cel <= 90)
+    {
+      analogWrite(V,255);
+      delay(1000);
       analogWrite(V,0);
-  }
+    }
   }
 }
