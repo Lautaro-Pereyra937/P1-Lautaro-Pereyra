363fe800fa38b02957ee7b5c376fe19d321a8876
Lautaro Pereyra <lautaro.pereyraaaet32@gmail.com>
2026-06-06 13:56:07 -0300

    Update brave_blad1.ino

--- brave_blad1.ino
+++ brave_blad1.ino
@@ -1,10 +1,12 @@
 #define A 5
+#define R 6
+#define V 3
 #define valortemp 2
 #define sensor 7
 #define valorpoten A1
 bool movimiento = LOW;
-float potenciometro = 0;
-float temperatura = 0;
+int potenciometro = 0;
+int temperatura = 0;
 
 void setup()
 {
@@ -12,19 +14,24 @@ void setup()
   pinMode(movimiento, INPUT);
   pinMode(valortemp, INPUT);
   pinMode(potenciometro, INPUT);
-  pinMode(valorpoten, INPUT);
 }
 
 void loop()
 {
   movimiento = digitalRead(sensor);
-  potenciometro = digitalRead(valorpoten);
-  temperatura = digitalRead(valortemp);
+  potenciometro = analogRead(valorpoten);
+  potenciometro = map(potenciometro,0, 1023, 0, 100);
+
+  temperatura = analogRead(valortemp);
+  temperatura = map(((temperatura - 20) * 3.04), 0, 1023, -40, 125);
+  
   if(potenciometro > 50 && temperatura > 20)
   {
     if(movimiento == HIGH)
     {
-      analogWrite(A , 200);
+      analogWrite(R , 64);
+      analogWrite(V, 224);
+      analogWrite(A, 208);
       delay(100);
     }
     else
